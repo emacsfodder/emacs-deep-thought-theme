8f4811ec3bdcd28b10fa05c943ad499b97b50bf8
JasonM23 <jasonm23@gmail.com>
2013-02-22 21:49:14 +1100

    Update deep-thought-theme.el

--- deep-thought-theme.el
+++ deep-thought-theme.el
@@ -2,7 +2,7 @@
 ;;;
 ;;; Author: Jason Milkins <jasonm23@gmail.com>
 ;;; Url: https://github.com/jasonm23/emacs-deep-thought-theme
-;;; Version: 0.0.1
+;;; Version: 0.0.2
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
